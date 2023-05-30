68b367841db897ef0e8237ac0671bfe524b247dd
Henrique Santos <107014801+HenriqueSantosMarcelino@users.noreply.github.com>
2023-05-30 14:31:26 -0300

    responsividade na agenda

--- js/temporizador.js
+++ js/temporizador.js
@@ -1,49 +1,42 @@
-// Defina as datas de conclusão das tarefas
-var tarefa1Data = new Date('2023-06-01');
-var tarefa2Data = new Date('2023-06-15');
+var semana1CSS = new Date('2023-05-22');
+var semana2CSS = new Date('2023-06-05');
 
-// Função para calcular o tempo restante
 function calcularTempoRestante(dataFinal) {
   var agora = new Date().getTime();
   var diferenca = dataFinal - agora;
 
   var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
   var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
-  var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
 
   return {
     'dias': dias,
     'horas': horas,
-    'minutos': minutos,
-    'segundos': segundos
   };
 }
 
-// Função para exibir o tempo restante
 function exibirTempoRestante(dataFinal, elemento) {
   var timerElemento = document.getElementById(elemento);
 
   function atualizarTempo() {
     var tempoRestante = calcularTempoRestante(dataFinal);
 
-    timerElemento.innerHTML = 'Tempo restante: ' + tempoRestante.dias + ' dias, ' +
-                              tempoRestante.horas + ' horas, ' +
-                              tempoRestante.minutos + ' minutos, ' +
-                              tempoRestante.segundos + ' segundos';
+    timerElemento.innerHTML =
+                              '<div class="timer"><div class="timer__titulo">Estou dentro do <br><h3>Cronograma?</h3></div>' 
+                             + '<div class="timer__restante"><div class="timer__restante__diashoras">' + tempoRestante.dias + ' </div><div>Dia(s)</div></div> '
+                             + ' <div class="timer__restante"><div class=timer__restante__diashoras> ' + tempoRestante.horas + '</div><div>Hora(s)</div></div></div> ';
 
-    // Verifica se o tempo já chegou a zero
-    if (tempoRestante.dias <= 0 && tempoRestante.horas <= 0 && tempoRestante.minutos <= 0 && tempoRestante.segundos <= 0) {
-      timerElemento.innerHTML = 'Tarefa concluída!';
+    // verifica se o tempo já chegou a zero
+    if (tempoRestante.dias <= 0 && tempoRestante.horas <= 0) {
+      timerElemento.innerHTML = '<div class="timer__finalizado"><div class="timer__titulo__finalizado">Estou dentro do <br><h3>Cronograma?</h3></div>' 
+                              + '<div class="timer__restante__finalizado"><div class="timer__restante__diashoras__finalizado"> 0 </div><div>Dia(s)</div></div> '
+                              + ' <div class="timer__restante__finalizado"><div class=timer__restante__diashoras__finalizado> 0 </div><div>Hora(s)</div></div></div> '; 
       clearInterval(intervalID);
     }
   }
 
-  // Atualiza o tempo inicialmente e, em seguida, a cada segundo
   atualizarTempo();
   var intervalID = setInterval(atualizarTempo, 1000);
 }
 
-// Chama a função para exibir o tempo restante para cada tarefa
-exibirTempoRestante(tarefa1Data, 'timer');
-exibirTempoRestante(tarefa2Data, 'timer');
+exibirTempoRestante(semana1CSS, 'semana1CSS');
+exibirTempoRestante(semana2CSS, 'semana2CSS');
